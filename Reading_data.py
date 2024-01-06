7a60546b2cf4b166c2816d2f53ac4033f9b248a3
Santiago Rivera <santiagor10764@gmail.com>
2024-01-05 19:29:08 -0500

    Read the CSV file, organized dataframe to focus on Philadelphia, PA

--- Reading_data.py
+++ Reading_data.py
@@ -4,3 +4,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
 
+#Load csv file into a dataframe
+House_Information = pd.read_csv("City_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv")
+
+print(House_Information.head())
+
+print(House_Information.describe())
+print(House_Information.RegionName.unique)
+print(House_Information.RegionName.nunique)
+
+
+Philadelphia_Values = House_Information.loc[House_Information.RegionName == 'Philadelphia'] 
+Philadelphia_Value = Philadelphia_Values.loc[House_Information.State == "PA"]
+print(Philadelphia_Value)
+
+
